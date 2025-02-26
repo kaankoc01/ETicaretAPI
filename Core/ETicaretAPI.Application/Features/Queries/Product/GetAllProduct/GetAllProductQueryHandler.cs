57e50fb115f2c94334687e156109bce56c97ac18
kaan koc <kaankoc01@gmail.com>
2025-02-26 15:30:02 +0300

    backend global exception handler

--- Core/ETicaretAPI.Application/Features/Queries/Product/GetAllProduct/GetAllProductQueryHandler.cs
+++ Core/ETicaretAPI.Application/Features/Queries/Product/GetAllProduct/GetAllProductQueryHandler.cs
@@ -18,6 +18,7 @@ namespace ETicaretAPI.Application.Features.Queries.Product.GetAllProduct
         public async Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Tüm ürünler getirildi. , get all products");
+            
             var totalCount = _productReadRepository.GetAll(false).Count();
             var products = _productReadRepository.GetAll(false).Skip(request.Page * request.Size).Take(request.Size).Select(p => new
             {
